c9dd9538d00fe19292b6fbfa216d18fdc5b2c9b1
Nikolaj Rønne <nikolajron@gmail.com>
2020-03-09 10:53:01 +0100

    no change

--- init.el
+++ init.el
@@ -23,7 +23,7 @@
  ;; If there is more than one, they won't work right.
  '(package-selected-packages
    (quote
-    (flycheck which-key use-package try org-bullets help-find-org-mode dash counsel))))
+    (expand-region flycheck which-key use-package try org-bullets help-find-org-mode dash counsel))))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
