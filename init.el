4abb6de26f77e0a687fb7cb895919ea1930cd11a
Nikolaj Rønne <nikolajron@gmail.com>
2020-03-10 23:14:49 +0100

    keymap for MacOS working with autopair

--- init.el
+++ init.el
@@ -28,7 +28,7 @@
  ;; If there is more than one, they won't work right.
  '(package-selected-packages
    (quote
-    (doom-modeline autopair expand-region flycheck which-key use-package try org-bullets help-find-org-mode dash counsel))))
+    (wrap-region doom-modeline expand-region flycheck which-key use-package try org-bullets help-find-org-mode dash counsel))))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
