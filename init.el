9a278beb7941774666c83cca96d0c1e4e5183bfd
Nikolaj Rønne <nikolajron@gmail.com>
2020-03-13 11:26:35 +0100

    * ./: Custom leuven mode-line theme color

--- init.el
+++ init.el
@@ -26,9 +26,18 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
+ '(ansi-color-faces-vector
+   [default default default italic underline success warning error])
+ '(ansi-color-names-vector
+   ["black" "red3" "ForestGreen" "yellow3" "blue" "magenta3" "DeepSkyBlue" "gray50"])
+ '(custom-safe-themes
+   (quote
+    ("672bb062b9c92e62d7c370897b131729c3f7fd8e8de71fc00d70c5081c80048c" "76c5b2592c62f6b48923c00f97f74bcb7ddb741618283bdb2be35f3c0e1030e3" default)))
+ '(debug-on-error t)
+ '(hl-sexp-background-color "#efebe9")
  '(package-selected-packages
    (quote
-    (wrap-region doom-modeline expand-region flycheck which-key use-package try org-bullets help-find-org-mode dash counsel))))
+    (all-the-icons-ibuffer leuven-theme zenburn-theme wrap-region doom-modeline expand-region flycheck which-key use-package try org-bullets help-find-org-mode dash counsel))))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
