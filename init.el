bd334400342ed1102beff4ada234241ca832757f
nronne <nikolajron@gmail.com>
2023-03-19 17:22:48 +0100

    added init.el to ignore

--- init.el
+++ init.el
@@ -29,3 +29,17 @@
 (org-babel-load-file (expand-file-name "~/.emacs.d/config.org"))
 
 
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(custom-safe-themes
+   '("00cec71d41047ebabeb310a325c365d5bc4b7fab0a681a2a108d32fb161b4006" "7e068da4ba88162324d9773ec066d93c447c76e9f4ae711ddd0c5d3863489c52" default)))
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(flymake-error ((((class color)) (:underline "red"))))
+ '(flymake-warning ((((class color)) (:underline "dim grey")))))
